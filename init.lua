c122824a0dc76ad8965c490f5f255f6b68aeaba7
rightfish <alvinttel@gmail.com>
2024-03-12 20:44:22 +0800

    *

--- init.lua
+++ init.lua
@@ -3,11 +3,10 @@ require('settings')
 require('auto_on')
 require('language/init')
 require('maps')
-require('statusline')     -- lua/statusline.lua
+require('statusline') -- lua/statusline.lua
 require('theme')
 
 -- require('plugins/airline')
 -- require('plugins/nvim-tree_tree')
 
 --require('plugins/bufferline_')
-
