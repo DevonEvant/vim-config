077e53704d1f3b55dfecbee8b395663881ba1def
rightfish <alvinttel@gmail.com>
2024-04-12 16:45:05 +0800

    *

--- lua/plugins/nvim-tree_tree.lua
+++ lua/plugins/nvim-tree_tree.lua
@@ -6,45 +6,47 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.nvim_tree_gitignore = 0
 
 require("nvim-tree").setup({
-        hijack_cursor = true,
-        sort = {
-            sorter = "case_sensitive",
-        },
-        view = {
-            width = 30,
-        },
-        renderer = {
-            group_empty = true,
-            icons = {
-                show = {
-                    git = true,
-                    file = true,
-                    folder = true,
-                    folder_arrow = true,
+    hijack_cursor = true,
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+        icons = {
+            show = {
+                git = true,
+                file = true,
+                folder = true,
+                folder_arrow = true,
+            },
+            glyphs = {
+                bookmark = " ",
+                folder = {
+                    arrow_closed = "⏵",
+                    arrow_open = "⏷",
                 },
-                glyphs = {
-                    bookmark = " ",
-                    folder = {
-                        arrow_closed = "⏵",
-                        arrow_open = "⏷",
-                    },
-                    git = {
-                        unstaged = "✗",
-                        staged = "✓",
-                        unmerged = "⌥",
-                        renamed = "➜",
-                        untracked = "★",
-                        deleted = "⊖",
-                        ignored = "◌",
-                    },
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "⌥",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "⊖",
+                    ignored = "◌",
                 },
             },
         },
-        filters = {
-            dotfiles = false,
-            git_clean = false,
-            no_buffer = false,
-            custom = { 'node_modules' },
-        },
-    })
-
+    },
+    filters = {
+        dotfiles = false,
+        git_clean = false,
+        no_buffer = false,
+        -- custom = {
+            -- "node_modules",
+            -- ".git/"
+        -- },
+    },
+})
