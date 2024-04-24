3894986d0200f104ed45b98816e9d276171cdbb0
rightfish <alvinttel@gmail.com>
2024-04-24 16:08:34 +0800

    *

--- lua/lazy_nvim.lua
+++ lua/lazy_nvim.lua
@@ -204,7 +204,23 @@ LazyVim.setup({
     },
 
 
-    { 'mbledkowski/neuleetcode.vim' },
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim", -- required by telescope
+            "MunifTanjim/nui.nvim",
+
+            -- optional
+            "nvim-treesitter/nvim-treesitter",
+            "rcarriga/nvim-notify",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            -- configuration goes here
+        },
+    },
 
     { 'LunarVim/bigfile.nvim' },
 
