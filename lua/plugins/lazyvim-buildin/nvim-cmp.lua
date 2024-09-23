2bd405816522e5f6cd7366bb992852b52d45e5d1
noi <horningch@gmail.com>
2024-09-23 09:43:57 +0800

    *

--- lua/plugins/lazyvim-buildin/nvim-cmp.lua
+++ lua/plugins/lazyvim-buildin/nvim-cmp.lua
@@ -1,6 +1,9 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+
+    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local luasnip = require("luasnip")
       local cmp = require("cmp")
@@ -8,40 +11,54 @@ return {
       -- preselect = cmp.PreselectMode.None,
       -- table.insert(opts.mapping, {
 
-      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          -- if cmp.get_active_entry() == 1 then
-          --     cmp.select_next_item()
-          --     cmp.select_prev_item()
-          -- end
-          cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-        elseif luasnip.expandable() then
-          luasnip.expand()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif check_backspace() then
-          fallback()
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
+      table.insert(opts.sources, { name = "emoji" })
+      table.insert(opts.sources, { name = "nvim_lsp" })
+      table.insert(opts.sources, { name = "luasnip" })
+      table.insert(opts.sources, { name = "buffer" })
+      table.insert(opts.sources, { name = "path" })
+
+      local check_backspace = function()
+        local col = vim.fn.col(".") - 1
+        return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+      end
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- if cmp.get_active_entry() == 1 then
+            --     cmp.select_next_item()
+            --     cmp.select_prev_item()
+            -- end
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif check_backspace() then
+            fallback()
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
       })
 
-      opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      })
-      -- })
       table.insert(opts.sources, {
         { name = "nvim_lsp" },
         -- { name = 'vsnip' }, -- For vsnip users.
