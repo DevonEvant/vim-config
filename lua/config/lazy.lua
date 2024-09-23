3f77cc887af0be7b95a5989e2923c6503faabca5
noi <horningch@gmail.com>
2024-09-23 09:48:39 +0800

    *

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     --
     { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- { import = "lazyvim.plugins.extras.coding.codeium" },
     --
     { import = "lazyvim.plugins.extras.editor.fzf" },
