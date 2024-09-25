1d0544235a50afbfc0c80f8c9fd03ce33ec9877d
noi <horningch@gmail.com>
2024-09-25 12:46:44 +0800

    *

--- lua/plugins/tools/toggleterm.lua
+++ lua/plugins/tools/toggleterm.lua
@@ -7,6 +7,7 @@ return {
       local buf_map = vim.api.nvim_buf_set_keymap
 
       map("n", "<A-t>", ":ToggleTerm direction=float<CR><ESC>", {})
+      map("i", "<A-t>", ":ToggleTerm direction=float<CR><ESC>", {})
     end,
   },
 }
