e4b9dbb97c1400f79081ad7849144288f6af5aec
noi <horningch@gmail.com>
2024-09-25 10:57:28 +0800

    *

--- lua/plugins/tools/toggleterm.lua
+++ lua/plugins/tools/toggleterm.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			local map = vim.api.nvim_set_keymap
-			local buf_map = vim.api.nvim_buf_set_keymap
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = function(_, opts)
+      local map = vim.api.nvim_set_keymap
+      local buf_map = vim.api.nvim_buf_set_keymap
 
-			map("n", "<A-t>", ":ToggleTerm direction=float<CR><ESC>", {})
-		end,
-	},
+      map("n", "<A-t>", ":ToggleTerm direction=float<CR><ESC>", {})
+    end,
+  },
 }
