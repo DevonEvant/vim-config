e5c0086b56ab74d906e34fbfd1e4eb63c5d5f68a
noi <horningch@gmail.com>
2024-09-30 09:35:02 +0800

    *

--- depend.sh
+++ depend.sh
@@ -1,4 +1,25 @@
-paru -S zeal
-paru -S ttf-firacode-nerd
-paru -S texlive-latex
+# paru -S zeal
+# paru -S ttf-firacode-nerd
+# paru -S texlive-latex
 paru -S wl-clipboard
+paru -S ripgrep
+paru -S fd
+paru -S fish
+# python
+pip install pynvim
+pip install neovim
+pip install debugpy
+#
+paru -S texlive
+paru -S latex2text
+paru -S biber
+#
+paru -S viu
+paru -S chafa
+paru -S ueberzug
+paru -S ripgrep
+
+pnpm install -g markdown-toc
+pnpm install -g markdownlint-cli2
+
+paru -S ueberzugpp
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     --
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- { import = "lazyvim.plugins.extras.coding.codeium" },
